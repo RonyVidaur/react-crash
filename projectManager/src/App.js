5064b2be2749bf032730b815732794865be58738
RonyVidaur <rony.vidaur@gmail.com>
2016-11-17 21:08:58 -0600

    submit handler

--- projectManager/src/App.js
+++ projectManager/src/App.js
@@ -26,10 +26,15 @@ class App extends Component {
       }
     ]})
   }
+  handleAddProject(project){
+    let projects = this.state.projects
+    projects.push(project)
+    this.setState({projects:projects})
+  }
   render() {
     return (
       <div className="App">
-        <AddProject />
+        <AddProject AddProject={this.handleAddProject.bind(this)} />
         <Projects projects={this.state.projects}/>
       </div>
     );
--- projectManager/src/components/add-project.js
+++ projectManager/src/components/add-project.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react'
 
 class AddProject extends Component {
+    constructor(){
+        super()
+        this.state = {
+            newProject: {
+
+            }
+        }
+    }
     static defaultProps = {
         categories: ['Web Design', 'Mobile Development']
     }
 
     handleSubmit(e){
         e.preventDefault()
-        console.log('submitted')
+        if(this.refs.title.value === ''){
+            alert("Title is required")
+        }else {
+          this.setState({newProject:{
+              title: this.refs.title.value,
+              category: this.refs.category.value
+          }}, function(){
+              //console.log(this.state)
+              this.props.AddProject(this.state.newProject)
+          })  
+        }
     }
     render(){
         let categoryOptions = this.props.categories.map(category => {
-            return <option key={category} value="category">{category}</option>
+            return <option key={category} value={category}>{category}</option>
         })
         return(
             <div>
                 <h3>Add Project</h3>
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.handleSubmit.bind(this)}>
                     <div>
                         <label>Title</label>
                         <input type="text" ref="title"/>
