29a15b5087d2d670be1f3f023708273e57e62262
RonyVidaur <rony.vidaur@gmail.com>
2016-11-21 00:03:34 -0600

    fetching data from external API

--- projectManager/src/App.js
+++ projectManager/src/App.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react'
 import uuid from 'uuid'
+import $ from 'jquery'
 import Projects from './components/projects'
 import AddProject from './components/add-project'
+import Todos from './components/todos'
 import './App.css';
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      projects: []
+      projects: [],
+      todos: []
     }
   }
-  componentWillMount(){
+  
+  getTodos(){
+    $.ajax({
+      url:'https://jsonplaceholder.typicode.com/todos',
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        this.setState({todos: data}, function(){
+          console.log(this.state)
+        })
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err)
+      }
+      
+    })
+  }
+  
+  getProjects(){
     this.setState({projects:[
       { id:uuid.v4(),
         title: "Bussiness Website",
@@ -26,6 +47,15 @@ class App extends Component {
         category: "Web Design"
       }
     ]})
+  }
+  
+  componentWillMount(){
+    this.getProjects()
+    this.getTodos()
+  }
+
+  componentDidMount(){
+    this.getTodos()
   }
 
   handleAddProject(project){
@@ -44,6 +74,8 @@ class App extends Component {
       <div className="App">
         <AddProject AddProject={this.handleAddProject.bind(this)} />
         <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)}/>
+        <hr/>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
